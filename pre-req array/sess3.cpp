ed52c16d688388b478f891174ea0ff3d779a0bfd
Anushka Singh <anushka019bteceai23@igdtuw.ac.in>
2026-01-24 21:10:46 +0530

    day 2

---
+++ pre-req array/sess3.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countpairBrute(const vector<int>&arr, int k){
+ int count = 0;
+ for(int i =0;i<arr.size();i++){
+  for(int j = i+1;j<arr.size(); j++){
+    if(arr[i]-arr[j] == k){
+      count++;
+    }
+  }
+ }
+ return count;
+}
+
+int countpairOptimized(const vector<int>&arr, int k){
+  unordered_map<int,int> freq; 
+  int count = 0;
+  for(int j = 0; j<arr.size();++j){
+    int comp = k + arr[j];
+    if(freq.find(comp)!= freq.end()){
+      count+=freq[comp];
+    }
+    freq[arr[j]]++;
+  }
+
+  return count;
+  
+}
+
+int main(){
+  int n;
+  cin>>n;
+  vector<int> arr(n);
+
+  for (int i = 0;i<n;i++){
+    cin>>arr[i];
+  }
+
+  int k;
+  cin >> k;
+  
+  cout<< countpairBrute(arr,k);
+  cout<< countpairOptimized(arr,k);
+
+}
+  
+
---
+++ pre-req array/sess4.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  vector<int> arr(n);
+  for(int i = 0;i<n;i++){
+    cin>>arr[i];
+  }
+  
+  int k;
+  cin>>k;
+  // brute force o(n2)
+  // int count = 0;
+  // for(int i = 0; i<n;i++){
+  //   for(int j = i+1;j<n;j++){
+  //      if (abs(arr[i]-arr[j])==k){
+  //       count++;
+  //      }
+  //   }
+  // }
+  // cout<< count;
+
+
+  //hashmap optimized
+
+  
+
+}
